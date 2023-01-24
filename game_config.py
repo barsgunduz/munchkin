0c5b707b458b7b95cae071fd1353ba9ae6108751
Bars Gunduz <abgunduz92@gmail.com>
2023-01-24 21:11:59 +0100

    Initial Commit

---
+++ game_config.py
@@ -0,0 +1,4 @@
+MonsterTypes=["Taksici", "Mutahit", "Galerici", "Hipster", "Zargana"]
+
+ItemTypes = ["Fiat Linea", "Tofas Sahin", "Torpil", "Memur Maasi", "Kirada Ev", "Tesla Model X", "Asiret Destegi"]
+
