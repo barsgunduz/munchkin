34c1a197af191d9ace1bb9384f607aee2fc6d8e3
Bars Gunduz <abgunduz92@gmail.com>
2023-01-28 21:21:07 +0100

    Moved the functionality completely to the ArenaController class

--- classes.py
+++ classes.py
@@ -12,18 +12,26 @@ class ArenaController:
         self.enemy_side = []
         self.allies_side = []
 
+    def shuffleDeck(self, array):
+        new = []
+        while len(array)>0:
+            new.append(array.pop(random.randint(0,len(array)-1)))
+        return new
+
     def createItemsDeck(self):
         ItemTypes = game_config.ItemTypes
         for i in range(0,self.size):
             self.items_deck.append(Item(ItemTypes[i%len(ItemTypes)][0],
                 ItemTypes[i%len(ItemTypes)][1]))
-    
+        self.items_deck = self.shuffleDeck(self.items_deck)
+        
     def createMonstersDeck(self):
         MonsterTypes = game_config.MonsterTypes
         for i in range(0,self.size):
             self.monsters_deck.append(Player(name = MonsterTypes[i%len(MonsterTypes)][0],
                 power = MonsterTypes[i%len(MonsterTypes)][1],
                 description = MonsterTypes[i%len(MonsterTypes)][2]))
+        self.monsters_deck = self.shuffleDeck(self.monsters_deck)
 
     def createPlayers(self, name = "DefaultPlayer", power = 1, type = "PLAYER"):
         self.player = Player(name, power, type)
@@ -47,8 +55,50 @@ class ArenaController:
         print("On the enemy side weeee haveeeee")
         for enemy in self.enemy_side:
             enemy.describePlayer()
-        
+
         print("Aaaand on the allieeess siideee, weeee haaaveee")
         for player in self.allies_side:
             player.describePlayer()
+
+    def playerRunsAway(self):
+        # Roll a dice and decide if the player dies or runs...
+        print("We will roll a dice for you. If the number is equal to 5 or higher, you will survive. Else... You will lose all your levels.")
+        input("Press ENTER to continue...")
+        dice = random.randint(1,6)
+        if dice >= 5:
+            print("The number is " + str(dice) + ". You are free to go...")
+        else:
+            print("The number is " + str(dice) + ". You lost your levels.")
+            for player in self.allies_side:
+                player.level = 1
+
+    def playerFights(self):
+        # Calculate the Power of Monster side
+        total_monster_power = 0
+        for monster in self.enemy_side:
+            total_monster_power += monster.power
+        print("Total power of the Monsters: " + str(total_monster_power))
+
+        # Calculate the power of Player side
+        total_player_power = 0
+        for player in self.allies_side:
+            total_player_power += player.getTotalPower()
+        print("Total power of the players: " + str(total_player_power))
+
+        # Decide on the winner side
+        if total_player_power > total_monster_power:
+            # Player wins
+            print(self.allies_side[0].name + " killed " + self.enemy_side[0].name)
+            # Player gets a card
+            self.allies_side[0].addItem(self.items_deck.pop(0))
+
+    def askFirstChoice(self):
+        print("You have 2 options:")
+        print("1) RUN")
+        print("2) FIGHT")
+        choice = input("Your choice: ")
+        if choice == "1":
+            self.playerRunsAway()
+        elif choice == "2":
+            self.playerFights()
 
--- main.py
+++ main.py
@@ -41,35 +41,5 @@ arena.describeArena()
 # THE FIGHT
 #################################################
 
-choice = input("You have 2 options: 1)RUN or 2)FIGHT\nYour choice: ")
-
-if choice == "1":
-    # Roll a dice and decide if the player dies or runs...
-    print("We will roll a dice for you. If the number is equal to 5 or higher, you will survive. Else... You will lose all your levels.")
-    input("Press ENTER to continue...")
-    dice = random.randint(1,6)
-    if dice >= 5:
-        print("The number is " + str(dice) + ". You are free to go...")
-    else:
-        print("The number is " + str(dice) + ". You lost your levels.")
-        player.level = 1
-elif choice == "2":
-    # Calculate the Power of Monster side
-    total_monster_power = 0
-    for monster in arena[0]:
-        total_monster_power += monster.power
-    print("Total power of the Monsters: " + str(total_monster_power))
-
-    # Calculate the power of Player side
-    total_player_power = 0
-    for player in arena[1]:
-        total_player_power += player.getTotalPower()
-    print("Total power of the players: " + str(total_player_power))
-
-    # Decide on the winner side
-    if total_player_power > total_monster_power:
-        # Player wins
-        print(player.name + " killed " + arena[0][0].name)
-        # Player gets a card
-        player.addItem(items.pop(0))
+arena.askFirstChoice()
     
